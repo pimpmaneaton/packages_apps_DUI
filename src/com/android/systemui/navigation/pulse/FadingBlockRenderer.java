c9519677e7141718d66518c2678bd555e065fda2
ezio84 <brabus84@gmail.com>
2018-02-07 14:17:15 +0100

    Pulse: don't lose color when changing render mode
    Change-Id: Idea74bdcc593d97460dafa61d7232ec08bd48fb5

--- src/com/android/systemui/navigation/pulse/FadingBlockRenderer.java
+++ src/com/android/systemui/navigation/pulse/FadingBlockRenderer.java
@@ -70,8 +70,12 @@ public class FadingBlockRenderer extends Renderer implements ColorAnimator.Color
     private boolean mLavaLampEnabled;
     private boolean mIsValidStream;
 
-    public FadingBlockRenderer(Context context, Handler handler, PulseObserver callback) {
+    private PulseController mController;
+
+    public FadingBlockRenderer(Context context, Handler handler, PulseObserver callback,
+            PulseController controller) {
         super(context, handler, callback);
+        mController = controller;
         mObserver = new LegacySettingsObserver(handler);
         mLavaLamp = new ColorAnimator();
         mLavaLamp.setColorAnimatorListener(this);
@@ -255,7 +259,8 @@ public class FadingBlockRenderer extends Renderer implements ColorAnimator.Color
                     mContext.getResources().getColor(R.color.config_pulseFillColor),
                     UserHandle.USER_CURRENT);
             if (!mLavaLampEnabled) {
-                mPaint.setColor(mAutoColor && mAlbumColor != -1 ? mAlbumColor : mUserColor);
+                int lastColor = mController.getAlbumArtColor();
+                mPaint.setColor(mAutoColor && lastColor != -1 ? lastColor : mUserColor);
             }
             int time = Settings.Secure.getIntForUser(resolver,
                     Settings.Secure.FLING_PULSE_LAVALAMP_SPEED, 10000,
@@ -325,6 +330,7 @@ public class FadingBlockRenderer extends Renderer implements ColorAnimator.Color
         }
         if (mAutoColor && !mLavaLampEnabled) {
             mPaint.setColor(mAlbumColor != 1 ? mAlbumColor : mUserColor);
+            mController.setLastColor(mAlbumColor);
         }
     }
 }
